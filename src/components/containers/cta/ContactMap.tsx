44f3bdfb84caaf64e08b1df2168a52ed1cddb601
chetan <chetanlal453@gmail.com>
2025-09-17 21:38:54 +0530

    map changed

--- src/components/containers/cta/ContactMap.tsx
+++ src/components/containers/cta/ContactMap.tsx
@@ -3,7 +3,16 @@ import React from "react";
 const ContactMap = () => {
   return (
     <div className="google__map-area pt-120">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d30938.29203384682!2d90.41205809043723!3d23.810342878819508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1668285235016!5m2!1sen!2sbd"></iframe>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24122.08247991234!2d79.073929!3d21.145800!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c5e4c1e8f8a3%3A0x58b7d1e963d8b28a!2sNagpur%2C%20Maharashtra%2C%20India!5e0!3m2!1sen!2sin!4v1694950000000!5m2!1sen!2sin"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Nagpur Map"
+      ></iframe>
     </div>
   );
 };
